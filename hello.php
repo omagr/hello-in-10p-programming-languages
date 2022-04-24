b2c8d3ad8db4aebb341b4ec662ecd27c782916c9
om agarwal <omag.about@gmail.com>
2022-04-25 00:12:50 +0530

    v.1

---
+++ hello.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<html>
+    <h1><?php echo 'hello world'; ?></h1>
+</html>
---
+++ hello.scala
@@ -0,0 +1,7 @@
+object scala 
+{
+    def main(args: Array[String]) 
+    {
+        println("Hello World") 
+    }
+}
